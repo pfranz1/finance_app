f0da16835d5b0778489c64e5e0cdb9effcabff60
Peter Franz <pfranz@teisd.com>
2021-05-25 10:17:36 -0500

    Increased the adaptability of the app to different screen sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:finance_app/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './models/transaction.dart';
 import './widgets/new_transaction.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -50,6 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // Transaction(id: "t2", name: "New Hat", price: 100.00, date: DateTime.now())
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _transactions.where((element) {
       return element.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
@@ -88,6 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    final _isLandscape = mediaQuery.orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: [
@@ -97,25 +109,54 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     );
+
+    final txList = Container(
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            0.7,
+        child: TransactionList(_transactions, _removeTxById));
+
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.3,
-              child: Chart(_recentTransactions),
-            ),
-            Container(
-                height: (MediaQuery.of(context).size.height -
+            if (_isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Show Chart'),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    },
+                  )
+                ],
+              ),
+            if (!_isLandscape)
+              Container(
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
-                    0.7,
-                child: TransactionList(_transactions, _removeTxById)),
+                        mediaQuery.padding.top) *
+                    0.3,
+                child: Chart(_recentTransactions),
+              ),
+            if (!_isLandscape) txList,
+            if (_isLandscape)
+              _showChart
+                  ? Container(
+                      height: (mediaQuery.size.height -
+                              appBar.preferredSize.height -
+                              mediaQuery.padding.top) *
+                          0.7,
+                      child: Chart(_recentTransactions),
+                    )
+                  : txList,
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,52 +11,67 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _transactions.isEmpty
-        ? Column(
-            children: [
-              Text(
-                'No input recived yet',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 100,
-                child: Image.asset(
-                  './assets/images/waiting.png',
-                  fit: BoxFit.fill,
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return _transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No input recived yet',
+                  style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
-            ],
-          )
-        : ListView.builder(
-            itemCount: _transactions.length,
-            itemBuilder: (context, index) {
-              return Card(
-                  elevation: 3,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: FittedBox(
-                          child: Text('\$${_transactions[index].price}')),
-                    ),
-                    title: Text(
-                      '${_transactions[index].name}',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                        '${DateFormat.yMMMd().format((_transactions[index].date))}'),
-                    trailing: IconButton(
-                      onPressed: () {
-                        _removeController(_transactions[index].id);
-                      },
-                      color: Theme.of(context).errorColor,
-                      icon: Icon(
-                        Icons.delete,
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.5,
+                  child: Image.asset(
+                    './assets/images/waiting.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: _transactions.length,
+              itemBuilder: (context, index) {
+                return Card(
+                    elevation: 3,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: FittedBox(
+                            child: Text('\$${_transactions[index].price}')),
                       ),
-                    ),
-                  ));
-            });
+                      title: Text(
+                        '${_transactions[index].name}',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      subtitle: Text(
+                          '${DateFormat.yMMMd().format((_transactions[index].date))}'),
+                      trailing: MediaQuery.of(context).size.width > 450
+                          ? ElevatedButton.icon(
+                              onPressed: () {
+                                _removeController(_transactions[index].id);
+                              },
+                              icon: Icon(
+                                Icons.delete,
+                              ),
+                              label: Text('Delete'),
+                              style: ElevatedButton.styleFrom(
+                                primary: Theme.of(context).errorColor,
+                              ),
+                            )
+                          : IconButton(
+                              onPressed: () {
+                                _removeController(_transactions[index].id);
+                              },
+                              color: Theme.of(context).errorColor,
+                              icon: Icon(
+                                Icons.delete,
+                              ),
+                            ),
+                    ));
+              });
+    });
   }
 }
